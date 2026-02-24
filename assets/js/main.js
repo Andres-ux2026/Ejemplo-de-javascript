59ed76e7a2e936cb9a2864d6fbbc34c5f0789aa0
Andres-ux2026 <moraga_2006@hotmail.com>
2026-02-24 00:10:22 +0000

    avance javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,38 @@ pepito.textContent = "hola"
 pepito.addEventListener("mouseover", function(){
     pepito.style.color = "green"
 });
+
+//condicionales 
+let nota =7;
+if(nota >=4){
+    console.log("aprobado");
+}else{
+    console.log("reprobado")
+}
+
+//funciones 
+
+function saludar(apellido){
+    console.log("hola"+ apellido);
+}
+
+saludar("carlos")
+
+function esmayor(edad){
+    if (edad >= 18 && edad <=100){
+        console.log("puede ver el contenido")
+    }else { 
+        console.log("No estas vivo para ver el contenido")
+    }
+    if (edad < 18){
+        console.log("Menor de edad no puede ver el contenido")
+    }
+}
+
+esmayor("101")
+
+function minusamayus(texto){
+    console.log(texto.toUpperCase();
+}
+minusamayus("pepito")
+
